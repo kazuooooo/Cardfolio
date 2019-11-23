5e10b0656e28b4952147b59e83b72e809b2ebf3c
KazuyaMatsumoto <matsumotokazuya7@gmail.com>
2019-11-23 17:13:40 +0900

    :heavy_plus_sign: CardFlip Example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import { render } from 'react-dom';
+import React, { useState } from 'react';
+import { useSpring, animated as a } from 'react-spring';
+import './styles.css';
 
-const IndexPage = () => (
-  // eslint-disable-next-line react/jsx-filename-extension
-  <>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-  </>
-);
+function Card() {
+  const [flipped, set] = useState(false);
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
+  return (
+    <div onClick={() => set((state) => !state)}>
+      <a.div class="c back" style={{ opacity: opacity.interpolate((o) => 1 - o), transform }} />
+      <a.div class="c front" style={{ opacity, transform: transform.interpolate((t) => `${t} rotateX(180deg)`) }} />
+    </div>
+  );
+}
 
-export default IndexPage;
+export default Card;
