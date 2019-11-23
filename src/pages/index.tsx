bb6f7a5ebdeab98372f071918bcae6ddacd60f82
KazuyaMatsumoto <matsumotokazuya7@gmail.com>
2019-11-23 16:55:52 +0900

    :put_litter_in_its_place: Clean up

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IndexPage = () => (
+  // eslint-disable-next-line react/jsx-filename-extension
+  <>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+  </>
+);
+
+export default IndexPage;
