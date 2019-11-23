997de9d2d9e27ed258f96ed34e65ef3b4519c9fb
KazuyaMatsumoto <matsumotokazuya7@gmail.com>
2019-11-23 18:56:09 +0900

    :heavy_plus_sign: add emotion

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-typescript',
+    'gatsby-plugin-emotion',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
@@ -32,4 +33,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-};
+}
