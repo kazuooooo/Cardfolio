fd1a5b0da0b8adbbf5e5944475bcd3c96c0c2de5
KazuyaMatsumoto <matsumotokazuya7@gmail.com>
2019-11-24 06:59:06 +0900

    :heavy_plus_sign: add reset.css

--- src/style.ts
+++ src/style.ts
@@ -1,12 +1,9 @@
 import { css } from '@emotion/core'
+import emotionReset from 'emotion-reset'
 
-export const globalStyle = css`
-  html * {
-    font-family: vdl-gigamarujr, sans-serif;
-    font-weight: 500;
-    font-style: normal;
-  }
-`
+export enum Colors {
+  Text = '#323333'
+}
 
 export enum FontSize {
   Title = '24px',
@@ -16,4 +13,25 @@ export enum FontSize {
 }
 
 export enum Margins {
+  Side = '64px',
+  LittleRelated = '28px',
+  Related = '8px',
+  StronglyRelated = '4px'
 }
+
+export const globalStyle = css`
+  ${emotionReset}
+  html * {
+    font-family: vdl-gigamarujr, sans-serif;
+    font-weight: 500;
+    font-style: normal;
+    color: ${Colors.Text};
+    line-height: 2.0em;
+  }
+  a {
+    text-decoration: none;
+  }
+  h1 {
+    font-size: ${FontSize.Title};
+  }
+`
