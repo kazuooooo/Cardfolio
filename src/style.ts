a9d4de495bf0fd5745ff5c4121e2c02e196bcda1
KazuyaMatsumoto <matsumotokazuya7@gmail.com>
2019-11-26 23:59:31 +0900

    :white_check_mark: Adjust shadow

--- src/style.ts
+++ src/style.ts
@@ -17,4 +17,4 @@ export enum Margins {
   StronglyRelated = '4px'
 }
 
-export const shadow = '0 -3px 6px rgba(0, 0, 0, 0.3)'
+export const shadow = 'rgba(0, 0, 0, 0.48) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'
