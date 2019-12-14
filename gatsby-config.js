4dcd27a11132eb6293ebd9a77d6831e7194f76b9
KazuyaMatsumoto <matsumotokazuya7@gmail.com>
2019-12-14 14:08:40 +0900

    :heavy_plus_sign: Add analyzer

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,9 @@ module.exports = {
         path: `${__dirname}/src/data/`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyser-v2',
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
