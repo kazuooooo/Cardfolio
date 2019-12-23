02befa08ca5f0ed8efed16f273f9cf8cc89225ea
KazuyaMatsumoto <matsumotokazuya7@gmail.com>
2019-12-23 16:08:49 +0900

    :white_check_mark: change menu order

--- src/components/menuItems/index.tsx
+++ src/components/menuItems/index.tsx
@@ -6,16 +6,16 @@ import skillSetComponent from './skillset'
 
 export enum MenuItemKey {
   SelfIntroduction = 'selfIntroduction',
-  Works = 'works',
-  Contact = 'contact',
   Career = 'career',
+  Works = 'works',
   SkillSet = 'skillSet',
+  Contact = 'contact',
 }
 
 export default {
   [MenuItemKey.SelfIntroduction]: selfIntroductionComponent,
-  [MenuItemKey.Works]: worksComponent,
-  [MenuItemKey.Contact]: contactComponent,
   [MenuItemKey.Career]: careerComponent,
+  [MenuItemKey.Works]: worksComponent,
   [MenuItemKey.SkillSet]: skillSetComponent,
+  [MenuItemKey.Contact]: contactComponent,
 }
